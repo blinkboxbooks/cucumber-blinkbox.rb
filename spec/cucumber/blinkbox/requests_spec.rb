59bec88c57c180c877c8b2ab0d3c2e5db3646f12
Tom <thomasf@blinkbox.com>
2014-03-28 17:30:35 +0000

    Fix indentation

--- spec/cucumber/blinkbox/requests_spec.rb
+++ spec/cucumber/blinkbox/requests_spec.rb
@@ -12,45 +12,45 @@ require 'active_support/core_ext/string'
 
 describe "requests" do
 
-    describe "setting query parameters" do
+  describe "setting query parameters" do
 
-        before :each do
-            @request = Object.new
-            @request.extend(KnowsAboutApiRequests)
-        end
-
-        it "accepts a single value" do
-            @request.set_query_param("foo", 1)
-            @request.query["foo"].should eq(1)
-        end
-
-        it "accepts an array of values" do
-            @request.set_query_param("foo", [1, 2])
-            @request.query["foo"].should eq([1, 2])
-        end
-
-        it "accepts multiple single values" do
-            @request.set_query_param("foo", 1)
-            @request.set_query_param("foo", 2)
-            @request.query["foo"].should eq([1, 2])
-        end
-
-        it "appends arrays to existing arrays" do
-            @request.set_query_param("foo", [1, 2])
-            @request.set_query_param("foo", [3, 4])
-            @request.query["foo"].should eq([1, 2, 3, 4])
-        end
-
-        it "appends single values to existing arrays" do
-            @request.set_query_param("foo", [1, 2])
-            @request.set_query_param("foo", 3)
-            @request.query["foo"].should eq([1, 2, 3])
-        end
-
-        it "appends arrays to existing values" do
-            @request.set_query_param("foo", 1)
-            @request.set_query_param("foo", [2, 3])
-            @request.query["foo"].should eq([1, 2, 3])
-        end
+    before :each do
+      @request = Object.new
+      @request.extend(KnowsAboutApiRequests)
     end
+
+    it "accepts a single value" do
+      @request.set_query_param("foo", 1)
+      @request.query["foo"].should eq(1)
+    end
+
+    it "accepts an array of values" do
+      @request.set_query_param("foo", [1, 2])
+      @request.query["foo"].should eq([1, 2])
+    end
+
+    it "accepts multiple single values" do
+      @request.set_query_param("foo", 1)
+      @request.set_query_param("foo", 2)
+      @request.query["foo"].should eq([1, 2])
+    end
+
+    it "appends arrays to existing arrays" do
+      @request.set_query_param("foo", [1, 2])
+      @request.set_query_param("foo", [3, 4])
+      @request.query["foo"].should eq([1, 2, 3, 4])
+    end
+
+    it "appends single values to existing arrays" do
+      @request.set_query_param("foo", [1, 2])
+      @request.set_query_param("foo", 3)
+      @request.query["foo"].should eq([1, 2, 3])
+    end
+
+    it "appends arrays to existing values" do
+      @request.set_query_param("foo", 1)
+      @request.set_query_param("foo", [2, 3])
+      @request.query["foo"].should eq([1, 2, 3])
+    end
+  end
 end
