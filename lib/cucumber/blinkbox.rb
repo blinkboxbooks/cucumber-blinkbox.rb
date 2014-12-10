a0e8805be997635c4f3feb5709a01b356af8e8e4
Muz <muz@blinkbox.com>
2014-12-10 16:42:19 +0000

    Add a general require's file to avoid having projects need to declare requiring each module individually if applicable

---
+++ lib/cucumber/blinkbox.rb
@@ -0,0 +1,6 @@
+require "cucumber/blinkbox/environment"
+require "cucumber/blinkbox/data_dependencies"
+require "cucumber/blinkbox/subjects"
+require "cucumber/blinkbox/requests"
+require "cucumber/blinkbox/responses"
+require "cucumber/blinkbox/response_validation"
